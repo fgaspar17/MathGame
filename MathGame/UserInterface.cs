a9e8a3c41ac1afa90ef6f954bd1ae3911d752fc2
Francisco Gaspar <nectryk@gmail.com>
2024-08-02 19:16:58 +0200

    user interface show menu

---
+++ MathGame/UserInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    public enum Options { Addition, Substraction, Multiplication, Division, History, Quit }
+    internal class UserInterface
+    {
+        public static Options ShowMenu()
+        {
+            
+            do
+            {
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Substraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. History");
+                Console.WriteLine("6. Quit");
+            } while (true);
+        }
+    }
+}
