c8def68c8dc48c3e6fc4ef7dbf56dac35417b1da
Francisco Gaspar <nectryk@gmail.com>
2024-08-03 10:10:07 +0200

    present operations and validation of the operation

---
+++ MathGame/MathOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    // TODO: Convertir en interface
+    internal class MathOperations
+    {
+        public int FirstOperand { get; init; }
+        public int SecondOperand { get; init; }
+
+        // TODO: Añadir dificultad en el Next()
+        public static MathOperations GetOperation(Options option)
+        {
+            return option switch
+            {
+                Options.Addition or Options.Multiplication or Options.Subtraction =>
+                    new MathOperations() { FirstOperand = GlobalRandom.Instance.Next(0, 101), SecondOperand = GlobalRandom.Instance.Next(0, 101) },
+                Options.Division =>
+                    GetDivision(),
+                _ => throw new InvalidEnumArgumentException($"The option {Enum.GetName(typeof(Options), option)} isn't valid")
+
+            };
+        }
+
+        // Divisor or SeconOperand between 80 and 100 and it must be an integer division
+        private static MathOperations GetDivision()
+        {
+            int dividend;
+            int divisor;
+
+            do
+            {
+                dividend = GlobalRandom.Instance.Next();
+                divisor = GlobalRandom.Instance.Next(80, 101);
+            }
+            while (dividend % divisor != 0);
+
+            return new MathOperations() { FirstOperand = dividend, SecondOperand = divisor };
+        }
+
+        public static int PerformOperation(Options option, MathOperations math)
+        {
+            int result;
+
+            switch (option)
+            {
+                case Options.Addition:
+                    result = math.FirstOperand + math.SecondOperand;
+                    break;
+                case Options.Subtraction:
+                    result = math.FirstOperand - math.SecondOperand;
+                    break;
+                case Options.Multiplication:
+                    result = math.FirstOperand * math.SecondOperand;
+                    break;
+                case Options.Division:
+                    result = math.FirstOperand / math.SecondOperand;
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException($"The option {Enum.GetName(typeof(Options), option)} isn't valid");
+            }
+
+            return result;
+        }
+    }
+}
--- MathGame/UserInterface.cs
+++ MathGame/UserInterface.cs
@@ -1,28 +1,86 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MathGame
 {
-    public enum Options { Unknown = 0, Addition = 1, Substraction = 2, Multiplication = 3, Division = 4, History = 5, Quit = 6, }
+    public enum Options { Unknown = 0, Addition = 1, Subtraction = 2, Multiplication = 3, Division = 4, History = 5, Quit = 6, }
     internal class UserInterface
     {
         public static Options ShowMenu()
         {
             Options optionSelected = new Options();
+            bool validSelection = false;
             do
             {
-                Console.WriteLine("1. Addition");
-                Console.WriteLine("2. Substraction");
-                Console.WriteLine("3. Multiplication");
-                Console.WriteLine("4. Division");
-                Console.WriteLine("5. History");
-                Console.WriteLine("6. Quit");
-            } while (InputValidator.MenuValidator(Console.ReadLine(), out optionSelected) == false);
+                DisplayMenuOptions();
+                Console.Write("Please enter a number (1-6): ");
+                validSelection = InputValidator.MenuValidator(Console.ReadLine(), out optionSelected);
+                if (!validSelection)
+                {
+                    Console.WriteLine("Invalid selection. Please try again.");
+
+                }
+
+            } while (!validSelection);
 
             return optionSelected;
+        }
+
+        private static void DisplayMenuOptions()
+        {
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction"); 
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. History");
+            Console.WriteLine("6. Quit");
+        }
+
+        // TODO: return a result object with the data of the game to save it
+        public static int ShowOperation(Options option, MathOperations math)
+        {
+            int result = 0;
+            bool validInput = false;
+            do
+            {
+                DisplayOperation(option, math);
+                Console.Write("Please enter the result of the operation: ");
+
+                validInput = InputValidator.OperationValidator(Console.ReadLine(), option, math);
+                if (!validInput)
+                {
+                    Console.WriteLine("Failed, try again!");
+                }
+
+            } while (!validInput);
+
+            return result;
+        }
+
+        public static void DisplayOperation(Options option, MathOperations math)
+        {
+            switch (option)
+            {
+                case Options.Addition:
+                    Console.WriteLine($"{math.FirstOperand} + {math.SecondOperand}");
+                    break;
+                case Options.Subtraction:
+                    Console.WriteLine($"{math.FirstOperand} - {math.SecondOperand}");
+                    break;
+                case Options.Multiplication:
+                    Console.WriteLine($"{math.FirstOperand} * {math.SecondOperand}");
+                    break;
+                case Options.Division:
+                    Console.WriteLine($"{math.FirstOperand} / {math.SecondOperand}");
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException($"The option {Enum.GetName(typeof(Options), option)} isn't valid");
+            }
         }
     }
 }
