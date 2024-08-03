067198a7775fe95021ff7b7a16b4417a4bda185a
Francisco Gaspar <nectryk@gmail.com>
2024-08-03 08:26:17 +0200

    input validation for the menu

---
+++ MathGame/InputValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal static class InputValidator
+    {
+        public static bool MenuValidator(string? input, out Options optionSelected)
+        {
+            optionSelected = Options.Unknown;
+            if (input == null) return false;
+
+            if (Options.TryParse<Options>(input.Trim(), out optionSelected))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- MathGame/UserInterface.cs
+++ MathGame/UserInterface.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace MathGame
 {
-    public enum Options { Addition, Substraction, Multiplication, Division, History, Quit }
+    public enum Options { Unknown = 0, Addition = 1, Substraction = 2, Multiplication = 3, Division = 4, History = 5, Quit = 6, }
     internal class UserInterface
     {
         public static Options ShowMenu()
         {
-            
+            Options optionSelected = new Options();
             do
             {
                 Console.WriteLine("1. Addition");
@@ -20,7 +20,9 @@ namespace MathGame
                 Console.WriteLine("4. Division");
                 Console.WriteLine("5. History");
                 Console.WriteLine("6. Quit");
-            } while (true);
+            } while (InputValidator.MenuValidator(Console.ReadLine(), out optionSelected) == false);
+
+            return optionSelected;
         }
     }
 }
